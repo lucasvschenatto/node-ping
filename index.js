f269806693eccfcc1792bfff272c37248a8d82a9
Lucas Schenatto <lucas.schenatto@sap.com>
2020-04-30 14:44:50 -0300

    setting default port

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
-let express = require('express')
-let app = express( )
+const express = require('express')
 
-app.get('*',(req,res) => {
-	res.send('Request received')
+let ports = process.argv.slice(2)
+if(!ports.length){
+	ports = [3478]
+}
+console.log(`Ports to listen are:${ports}`)
+
+ports.forEach((sPort) =>{	
+	let app = express( )
+	app.get('*',(req,res) => {
+		let message = `Request received on port: ${sPort}`
+		console.log(message)
+		res.send(message)
+	})
+	app.listen(parseInt(sPort),()=>{console.log(`listening on port ${sPort}`)})
 })
-
-app.listen(3478,()=>{console.log(`listening on port 3478`)})
